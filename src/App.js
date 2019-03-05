53cd5c3ee4d764fcc889b6da4dc7a837295f2d2d
Alexandr Tichon <tsikhan.p@gmail.com>
2019-03-05 16:46:59 +0300

    feat: check and save event

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import EventsContainer from './components/eventContainer/eventContainer';
+// import ControlBtns from './components/controlBtns/controlBtns';
 
 import './App.scss';
 
 const App = () => (
   <div className="App">
+    {/* <ControlBtns /> */}
     <EventsContainer />
   </div>
 );
