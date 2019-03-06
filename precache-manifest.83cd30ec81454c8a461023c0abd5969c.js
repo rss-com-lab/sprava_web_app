9008e9900a8b72606fa74cbfca4c76366c332d48
Alexandr Tichon <tsikhan.p@gmail.com>
2019-03-06 23:45:54 +0300

    Updates

--- precache-manifest.83cd30ec81454c8a461023c0abd5969c.js
+++ precache-manifest.83cd30ec81454c8a461023c0abd5969c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/sprava_web_app/static/js/runtime~main.6394e6c5.js"
   },
   {
-    "revision": "397772b64e988358420c",
-    "url": "/sprava_web_app/static/js/main.397772b6.chunk.js"
+    "revision": "56e1c5f4ec9dc3dba02d",
+    "url": "/sprava_web_app/static/js/main.56e1c5f4.chunk.js"
   },
   {
     "revision": "05ce65507a7ee78df3e6",
     "url": "/sprava_web_app/static/js/2.05ce6550.chunk.js"
   },
   {
-    "revision": "397772b64e988358420c",
+    "revision": "56e1c5f4ec9dc3dba02d",
     "url": "/sprava_web_app/static/css/main.e712619e.chunk.css"
   },
   {
-    "revision": "1fd11f9a3e9d39bfb38d88fd48ef7672",
+    "revision": "069f85f57c9faeccde2898b2a2971f80",
     "url": "/sprava_web_app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sprava_web_app/precache-manifest.a792fc585fc8b869c2f3ee33d9c62e70.js"
+  "/sprava_web_app/precache-manifest.83cd30ec81454c8a461023c0abd5969c.js"
 );
 
 workbox.clientsClaim();
