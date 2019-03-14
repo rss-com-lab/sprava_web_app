e07b6ecc1d9f2f8a3fa1a3ea04aad092505b40d4
Alexandr Tichon <tsikhan.p@gmail.com>
2019-03-14 20:51:12 +0300

    feat: subscr/unsubscr to events; update icons

--- src/containers/visibleEventList.js
+++ src/containers/visibleEventList.js
@@ -6,7 +6,28 @@ import eventsData from '../eventsData/eventsData';
 const getVisibleEvents = (filter) => {
   switch (filter) {
     case VisibilityFilters.SHOW_ALL:
-      return eventsData;
+    {
+      const localIdArray = (localStorage.getItem('checkedEventList'))
+        ? localStorage.getItem('checkedEventList').split(',').map(elem => parseInt(elem, 10))
+        : [];
+      const currentEventList = [];
+      if (localIdArray.length > 0) {
+        localIdArray.forEach((eventId) => {
+          const currentEvent = eventsData.find(event => event.id === eventId);
+          currentEvent.disable = true;
+          currentEventList.push(currentEvent);
+        });
+        eventsData.forEach((event) => {
+          if (!currentEventList.find(e => e.id === event.id)) {
+            const currentEvent = event;
+            currentEvent.disable = false;
+            currentEventList.push(currentEvent);
+          }
+        });
+        currentEventList.sort((event, nextEvent) => event.id - nextEvent.id);
+      }
+      return currentEventList;
+    }
     case VisibilityFilters.SHOW_SAVED:
     {
       const localIdArray = (localStorage.getItem('checkedEventList'))
