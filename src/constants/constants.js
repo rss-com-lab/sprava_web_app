65666b73f79ae33588eaa62c0c8d55e5cc66fba1
Alexandr Tichon <tsikhan.p@gmail.com>
2019-03-24 17:19:06 +0300

    feat: fetch function

---
+++ src/constants/constants.js
@@ -0,0 +1,10 @@
+export const SPRAVA_API_MAIN_URL = 'http://spravafestival.by/api/';
+
+export const SPRAVA_API_URL = {
+  music: 'music',
+  perfomance: 'performances',
+  actions: 'actions',
+  questions: 'questions',
+  partners: 'partners',
+  infopartners: 'infopartners',
+};
