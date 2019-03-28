3ee6f5bc7dc7f78e5084906e9760762e0114858e
Alexandr Tichon <tsikhan.p@gmail.com>
2019-03-28 20:02:08 +0300

    feat: update components on new api

--- src/components/eventList/eventList.js
+++ src/components/eventList/eventList.js
@@ -5,30 +5,59 @@ import Event from '../event/event';
 import './eventList.scss';
 
 const EventList = ({ events }) => (
-  <div className="events-section__event-list" id="event-list">
-    {events.map(e => (
-      <Event
-        key={e.id}
-        id={e.id}
-        eventName={e.eventName}
-        location={e.location}
-        time={e.time}
-        description={e.description}
-        isEventSave={Boolean(e.checked)}
-      />
-    ))}
+  <div className="events-section__event-list-container" id="event-list">
+    <div className="events-section__first-day-events">
+      <h5>4 АВГУСТА</h5>
+      <div className="events-section__first-day-events-list">
+        {(Object.keys(events)
+          .filter(e => events[e].date === '4 АВГУСТА'))
+          .map(eventNumber => (
+            <Event
+              key={events[eventNumber].id.match(/^\d+/)[0]
+                + events[eventNumber].id.match(/\d+$/)[0]}
+              img={events[eventNumber].img}
+              title={events[eventNumber].title}
+              schedule={events[eventNumber].schedule}
+              link={events[eventNumber].link}
+              id={events[eventNumber].id}
+              isEventSave={Boolean(events[eventNumber].checked)}
+            />
+          ))}
+      </div>
+    </div>
+    <div className="events-section__secound-day-events">
+      <h5>5 АВГУСТА</h5>
+      <div className="events-section__secound-day-events-list">
+        {(Object.keys(events)
+          .filter(e => events[e].date === '5 АВГУСТА'))
+          .map(eventNumber => (
+            <Event
+              key={events[eventNumber].id.match(/^\d+/)[0]
+              + events[eventNumber].id.match(/\d+$/)[0]}
+              img={events[eventNumber].img}
+              title={events[eventNumber].title}
+              schedule={events[eventNumber].schedule}
+              link={events[eventNumber].link}
+              id={events[eventNumber].id}
+              isEventSave={Boolean(events[eventNumber].checked)}
+            />
+          ))}
+      </div>
+    </div>
   </div>
 );
 
 EventList.propTypes = {
   events: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    eventName: PropTypes.string.isRequired,
-    location: PropTypes.string.isRequired,
-    time: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    schedule: PropTypes.objectOf(PropTypes.shape({
+      point: PropTypes.string.isRequired,
+      timeStart: PropTypes.string.isRequired,
+      timeEnd: PropTypes.string.isRequired,
+    })).isRequired,
+    link: PropTypes.string.isRequired,
   }).isRequired).isRequired,
 };
-
 
 export default EventList;
