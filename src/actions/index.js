4926dac6977490d3f4f4d4081288d85c58a43222
Alexandr Tichon <tsikhan.p@gmail.com>
2019-03-30 01:31:03 +0300

    feat: fetch music API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import fetch from 'cross-fetch';
+
 export const setVisibilityFilter = filter => ({
   type: 'SET_VISIBILITY_FILTER',
   filter,
@@ -7,3 +9,46 @@ export const VisibilityFilters = {
   SHOW_ALL: 'SHOW_ALL',
   SHOW_SAVED: 'SHOW_SAVED',
 };
+
+// action creators
+
+export function itemsHasErrored(bool) {
+  return {
+    type: 'ITEMS_HAS_ERRORED',
+    hasErrored: bool,
+  };
+}
+
+export function itemsIsLoading(bool) {
+  return {
+    type: 'ITEMS_IS_LOADING',
+    isLoading: bool,
+  };
+}
+
+export function itemsFetchDataSuccess(items) {
+  return {
+    type: 'ITEMS_FETCH_DATA_SUCCESS',
+    items,
+  };
+}
+
+export function itemsFetchData(url) {
+  return (dispatch) => {
+    dispatch(itemsIsLoading(true));
+
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+
+        dispatch(itemsIsLoading(false));
+
+        return response;
+      })
+      .then(response => response.json())
+      .then(items => dispatch(itemsFetchDataSuccess(items)))
+      .catch(() => dispatch(itemsHasErrored(true)));
+  };
+}
--- src/components/eventList/eventList.js
+++ src/components/eventList/eventList.js
@@ -4,50 +4,75 @@ import Event from '../event/event';
 
 import './eventList.scss';
 
-const EventList = ({ events }) => (
-  <div className="events-section__event-list-container" id="event-list">
-    <div className="events-section__first-day-events">
-      <h5>4 АВГУСТА</h5>
-      <div className="events-section__first-day-events-list">
-        {(Object.keys(events)
-          .filter(e => events[e].date === '4 АВГУСТА'))
-          .map(eventNumber => (
-            <Event
-              key={events[eventNumber].id.match(/^\d+/)[0]
+class EventList extends React.Component {
+  componentDidMount() {
+    const { fetchData } = this.props;
+    fetchData('https://cors-anywhere.herokuapp.com/http://spravafestival.by/api/music', { mode: 'cors' });
+  }
+
+  render() {
+    const {
+      events,
+      hasErrored,
+      isLoading,
+    } = this.props;
+    if (hasErrored) {
+      return <p>Sorry! There was an error loading the items</p>;
+    }
+
+    if (isLoading) {
+      return <p>Loading…</p>;
+    }
+
+    return (
+      <div className="events-section__event-list-container" id="event-list">
+        <div className="events-section__first-day-events">
+          <h5>4 АВГУСТА</h5>
+          <div className="events-section__first-day-events-list">
+            {(Object.keys(events)
+              .filter(e => events[e].date === '4 АВГУСТА'))
+              .map(eventNumber => (
+                <Event
+                  key={events[eventNumber].id.match(/^\d+/)[0]
                 + events[eventNumber].id.match(/\d+$/)[0]}
-              img={events[eventNumber].img}
-              title={events[eventNumber].title}
-              schedule={events[eventNumber].schedule}
-              link={events[eventNumber].link}
-              id={events[eventNumber].id}
-              isEventSave={Boolean(events[eventNumber].checked)}
-            />
-          ))}
-      </div>
-    </div>
-    <div className="events-section__secound-day-events">
-      <h5>5 АВГУСТА</h5>
-      <div className="events-section__secound-day-events-list">
-        {(Object.keys(events)
-          .filter(e => events[e].date === '5 АВГУСТА'))
-          .map(eventNumber => (
-            <Event
-              key={events[eventNumber].id.match(/^\d+/)[0]
+                  img={events[eventNumber].img}
+                  title={events[eventNumber].title}
+                  schedule={events[eventNumber].schedule}
+                  link={events[eventNumber].link}
+                  id={events[eventNumber].id}
+                  isEventSave={Boolean(events[eventNumber].checked)}
+                />
+              ))}
+          </div>
+        </div>
+        <div className="events-section__secound-day-events">
+          <h5>5 АВГУСТА</h5>
+          <div className="events-section__secound-day-events-list">
+            {(Object.keys(events)
+              .filter(e => events[e].date === '5 АВГУСТА'))
+              .map(eventNumber => (
+                <Event
+                  key={events[eventNumber].id.match(/^\d+/)[0]
               + events[eventNumber].id.match(/\d+$/)[0]}
-              img={events[eventNumber].img}
-              title={events[eventNumber].title}
-              schedule={events[eventNumber].schedule}
-              link={events[eventNumber].link}
-              id={events[eventNumber].id}
-              isEventSave={Boolean(events[eventNumber].checked)}
-            />
-          ))}
+                  img={events[eventNumber].img}
+                  title={events[eventNumber].title}
+                  schedule={events[eventNumber].schedule}
+                  link={events[eventNumber].link}
+                  id={events[eventNumber].id}
+                  isEventSave={Boolean(events[eventNumber].checked)}
+                />
+              ))}
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 EventList.propTypes = {
+  fetchData: PropTypes.func.isRequired,
+  hasErrored: PropTypes.bool.isRequired,
+  isLoading: PropTypes.bool.isRequired,
   events: PropTypes.arrayOf(PropTypes.shape({
     img: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
